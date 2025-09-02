c9da8774d2a202b7e79a6759af657bb1c79677c0
Facundo <faisher00@gmail.com>
2025-09-02 14:33:20 -0300

    first-commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import helmet from "helmet";
+import { AppModule } from "./app.module";
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle("NestJS Quickstart")
+    .setDescription("The NestJS Quickstart API description")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();

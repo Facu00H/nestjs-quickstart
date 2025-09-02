18d50945ae1a964be1541c424daea3394fe15a30
Facundo <faisher00@gmail.com>
2025-09-02 14:36:45 -0300

    format

--- src/main.ts
+++ src/main.ts
@@ -4,18 +4,18 @@ import helmet from "helmet";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule);
 
-  app.use(helmet());
+	app.use(helmet());
 
-  const config = new DocumentBuilder()
-    .setTitle("NestJS Quickstart")
-    .setDescription("The NestJS Quickstart API description")
-    .setVersion("1.0")
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api", app, document);
+	const config = new DocumentBuilder()
+		.setTitle("NestJS Quickstart")
+		.setDescription("The NestJS Quickstart API description")
+		.setVersion("1.0")
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api", app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
